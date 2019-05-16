3afc09e8c8989d13547c1dbeb2905df6428c8ba9
mikexx97 <50694909+mikexx97@users.noreply.github.com>
2019-05-16 09:46:54 +0200

    Create Badge.java

---
+++ Badge.java
@@ -0,0 +1,15 @@
+package nl.delphinity.pokemon.model.trainer;
+
+public class Badge {
+
+    private final String name;
+
+    public Badge(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
